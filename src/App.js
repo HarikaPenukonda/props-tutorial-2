1b697a445fea112c1a40dca18c6fdee96e34fdcf
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-11 16:08:57 -0500

    App render 4-5 <Joke/> components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Jokes from "./Jokes";
 
 export default function App(){
     return(
-        <h1>Hello World!</h1>
+        <div>
+            <Jokes
+                setup="I got my dauhter a fridge for her birthday."
+                punchline="I cant wait to see her face light up when she opens it"
+            />
+            <Jokes
+                setup="How did the hacker escape the police?"
+                punchline="He just ransomware!"
+            />
+            <Jokes
+                setup="Why dont pirates travel on mountain roads?"
+                punchline="Scurvy"
+            />
+            <Jokes
+                setup="Why do bees stay in the hive in the winter?"
+                punchline="Swarm"
+            />
+            <Jokes
+                setup="Whats the best thing about Switzerland?"
+                punchline="I dont know, but the flag is a big flag!"
+            />
+            <Jokes
+                punchline="Its hard to explain puns to kleptomaniacs because they always take things literally"
+            />
+        </div>
+        
+        
     )
 }
