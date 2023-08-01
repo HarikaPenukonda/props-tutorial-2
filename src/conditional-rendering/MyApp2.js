ba44c1a982d1bd4cd381899f9e2d0a6d728ec2e5
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-01 13:42:36 -0500

    conditional rendering practice

--- src/conditional-rendering/MyApp2.js
+++ src/conditional-rendering/MyApp2.js
@@ -13,7 +13,10 @@ export default function MyApp2(){
     return(
         <div>
         {
-            messages.length === 0 ? <h1>You are all caught up!</h1> : <h1>You have {messages.length} unread {messages.length>1 ? "messages" : "message"}</h1>
+            messages.length === 0 ? 
+                <h1>You are all caught up!</h1> : 
+                <h1>You have {messages.length} unread 
+                {messages.length>1 ? "messages" : "message"}</h1>
         }
         </div>
     )
